f4d00ae2f3266ca64ebb846545ed57ede7c15919
taehyung5271 <kth14200@gmail.com>
2026-02-20 16:55:24 +0900

    FeignClient(name = user-serivce > user-service 오타 수정

--- board-service/src/main/java/com/example/boardservice/client/UserClient.java
+++ board-service/src/main/java/com/example/boardservice/client/UserClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "user-serivce")
+@FeignClient(name = "user-service")
 public interface UserClient {
 
     @GetMapping("/api/users/{id}")
